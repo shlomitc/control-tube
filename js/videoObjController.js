0a1855b68ab73669dd6e5a66ed14fef1557e184a
Shlomi Toussia-Cohen <shlomi.t@supersonicads.com>
2014-11-22 21:11:16 +0200

    managed to inject scripts

--- js/videoObjController.js
+++ js/videoObjController.js
@@ -1,6 +1,7 @@
-(function(){
-    console.log('*** line[2] ***');
+(function (){
     var $ = window.jQuery;//pre-requisite
     var vid = $('[data-youtube-id]');
-    //todo - no vid? notify background page
-});
+
+    //notify background page
+    return vid ? true : false;
+})();
--- src/bg/background.js
+++ src/bg/background.js
@@ -19,22 +19,25 @@ var currentTabId = -1,
 
 window.onload = function () {
     var injectToVideoTab = function (tabId, tabIndex) {
-        console.log('*** line[22] ***', tabId,tabIndex);
+        console.log('*** line[22] ***', tabId, tabIndex);
         chrome.tabs.executeScript(tabId, {file: 'js/jquery-2.1.1.min.js'}, function (resultArrayJquery) {
-            if(!resultArrayJquery[0]){
-                //too bad...
-                console.log('Could not inject jquery');
-                return;
+            if (resultArrayJquery[0]) {
+                chrome.tabs.executeScript(tabId, {file: 'js/videoObjController.js'}, function (resultArrayController) {
+                    console.log('*** line[26] ***', resultArrayController);
+                    if (resultArrayController[0]) {
+                        currentTabId = tabId;
+                        currentTabIndex = tabIndex;
+                    }
+                    else {
+                        //too bad...
+                        console.log('Could not inject video controller');
+                    }
+                });
             }
-            chrome.tabs.executeScript(tabId, {file: 'js/videoObjController.js'}, function (resultArrayController) {
-                if(!resultArrayController[0]){
-                    //too bad...
-                    console.log('Could not inject video controller');
-                    return;
-                }
-                currentTabId = tabId;
-                currentTabIndex = tabIndex;
-            });
+            else {
+                //too bad...
+                console.log('Could not inject jQuery');
+            }
         });
     };
 
